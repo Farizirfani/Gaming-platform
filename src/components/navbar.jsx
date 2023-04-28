0342c08f474ae98a676d5549058f34f6bf1dae14
Farizirfani <farizirfani29@gmail.com>
2023-04-29 02:22:21 +0700

    add transparent navbar and spiderman banner

---
+++ src/components/navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+
+
+
+const Navbar = () => {
+
+
+        const [changeColor, setChangeColor] = useState(false);
+
+        const changeBackgroundColor = () => {
+            if (window.scrollY >= 1) {
+                setChangeColor(true);
+            } else {
+                setChangeColor(false);
+            }
+        };
+
+        useEffect(() => {
+            changeBackgroundColor();
+
+            window.addEventListener("scroll", changeBackgroundColor)
+        })
+
+
+return (
+    <>
+        <div className={changeColor ? `bg-active flex justify-between top-0 sticky xl:h-20 xl:py-4 xl:px-6` : 'bg-nonactive flex justify-between top-0 sticky xl:h-20 xl:py-4 xl:px-6'} >
+            <div className="logo text-white text-2xl font-bold">
+                <h1>Logo</h1>
+            </div>
+            <div className='flex justify-evenly gap-10 content-center'>
+                <div >
+                    <a className='flex justify-evenly text-white hover:border-b-4 border-indigo-600 border-solid' href="#a">testing</a>
+                </div>
+                <div className='flex justify-evenly text-white'>
+                    <h1>testing</h1>
+                </div>
+                <div className='flex justify-evenly text-white'>
+                    <h1>testing</h1>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,33 +1,50 @@
 import React from 'react'
 import JoyStick from '../../assets/image/joy_stick 1.png'
+import Spiderman from '../../assets/image/banner-spiderman.png'
+
 import Trending from '../../components/trendings'
+import Navbar from '../../components/navbar'
 
 const Home = () => {
   return (
     <div className='canvas-home'>
-        <h1 className='logo'>ini dari logo</h1>
-        <div className="container-home xl:px-32">
-            <div className="flex justify-between">
-                <div className='banner-home-1 w-1/3 content-center justify-center items-center'>
-                    <p className='text-ungu-1 font-extrabold text-lg tracking-tight xl:pb-2'>3D game dev</p>
-                    <h1 className='text-white font-bold text-5xl leading-relaxed'>Work that we produce for our clients</h1>
-                    <p className='text-white xl:my-7'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero dolore nostrum ullam illo! Quo provident molestiae facere rem, molestias incidunt.</p>
-                    <button className='text-white bg-gradient-1 p-3 rounded-full'>
-                        Get more details
-                    </button>
+        <div className="main-home">
+            <Navbar />
+            <div className="container-home xl:px-32">
+                <div className="flex justify-between">
+                    <div className='banner-home-1 w-1/3 content-center justify-center items-center'>
+                        <p className='text-ungu-1 font-extrabold text-lg tracking-tight xl:pb-2'>3D game dev</p>
+                        <h1 className='text-white font-bold text-5xl leading-relaxed'>Work that we produce for our clients</h1>
+                        <p className='text-white xl:my-7'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero dolore nostrum ullam illo! Quo provident molestiae facere rem, molestias incidunt.</p>
+                        <button className='text-white bg-gradient-1 p-3 rounded-full'>
+                            Get more details
+                        </button>
+                    </div>
+                    <div className='banner-home-2 w-1/2  justify-end items-end content-end flex z-1 -bottom-40'>
+                        <div className='w-4/6'>
+                            <img src={JoyStick} alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div className='banner-home-2 w-1/2  justify-end items-end content-end flex z-1 -bottom-40'>
-                    <div className='w-4/6'>
-                        <img src={JoyStick} alt="" />
+                <div  className="py-16" >
+                    <Trending/>
+                </div>
+                <div className='text-center px-40'>
+                    <h1 className=' text-white text-3xl font-semibold leading-loose'>Lorem ipsum dolor sit amet elit. Consectetur est expedita similique at corrupti possimus.</h1>
+                </div>
+                <div className='lg:py-16'>
+                    <div>
+                        <h1 className='text-white font-semibold text-3xl'>Lorem ipsum</h1>
+                        <p className='text-white w-1/2 font-thin leading-loose lg:py-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, nam numquam dolor consequatur harum odit delectus accusamus officia voluptatem a.</p>
+                    </div>
+                    <div className='lg:py-10'>
+                        <img src={Spiderman} alt="" />
                     </div>
                 </div>
             </div>
-            <div  className="py-16" >
-                <Trending/>
-            </div>
-            <div className='text-center px-40'>
-                <h1 className=' text-white text-3xl font-semibold leading-loose'>Lorem ipsum dolor sit amet elit. Consectetur est expedita similique at corrupti possimus.</h1>
-            </div>
+        </div>
+        <div className="utility-home bg-slate-800 h-auto">
+            <h1>testing cuyh</h1>
         </div>
     </div>
 )
