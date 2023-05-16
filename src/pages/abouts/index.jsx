b3a07fd470626b398ee3824ca0905c40d3ee8d4e
Farizirfani <farizirfani29@gmail.com>
2023-05-16 23:01:17 +0700

    add work-card

--- src/pages/abouts/index.jsx
+++ src/pages/abouts/index.jsx
@@ -11,12 +11,12 @@ const About = () => {
     <div className='canvas-about'>
         <div className="main-about h-auto">
             <Navbar />
-            <div className='h-auto'>
-              <div className="xl:flex xl:justify-between content-center ">
+            <div className='h-auto  px-7'>
+              <div className="xl:flex xl:justify-between content-center">
                 <div className='text-white xl:px-32 py-3 '>
                   <p className='py-4'> <Link to='/'>Home</Link>   {'>'} <span className='text-purple-900'> About us</span></p>
-                  <h1 className='text-3xl font-bold w-3/4 leading-relaxed'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, eveniet.</h1>
-                  <p className='w-1/2 font-light py-3'>Lorem ipsum dolor sit amet. Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quo, ab nostrum et totam eaque ullam beatae assumenda consequatur cupiditate?</p>
+                  <h1 className='md:text-3xl  text-lg font-bold w-3/4 leading-relaxed'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, eveniet.</h1>
+                  <p className='md:w-1/2 w-auto font-light py-3'>Lorem ipsum dolor sit amet. Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quo, ab nostrum et totam eaque ullam beatae assumenda consequatur cupiditate?</p>
                   <button className='bg-gradient-1 xl:my-5 px-3 py-2 rounded-full'>Get in touch <i class="fa-solid fa-arrow-right"></i></button>
                 </div>
                 <div className=''>
@@ -26,12 +26,13 @@ const About = () => {
                 </div>
               </div>
             </div>
-            <div className='py-16 xl:px-32'>
-              <h1 className='text-white font-semibold text-xl'>Why work with us</h1>
+            <div className='py-16 xl:px-32 px-7'>
+              <h1 className='text-white font-semibold text-xl sm:text-left text-center'>Why work with us</h1>
               <div>
                 <WorkCard />
               </div>
             </div>
+          <h1 className='text-white'>Hello brow ini dari about us</h1>
         </div>
     </div>
   )
--- src/pages/abouts/work_card.jsx
+++ src/pages/abouts/work_card.jsx
@@ -2,30 +2,29 @@ import React from 'react'
 
 const WorkCard = () => {
   return (
-    <div className='overflow-x-scroll flex flex-nowrap gap-10'>
-        <div className='w-40'>
-            testing
+  <>
+    <div className='md:flex flex-nowrap lg:justify-between md:justify-center py-3'>
+        <BoxWork title='lorem' header='lorem'  body='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, fuga. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, et?' />
+        <BoxWork title='lorem' header='lorem'  body='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, fuga.Lorem, sit amet consectetur adipisicing elit. Laudantium, et?' />
+        <BoxWork title='lorem' header='lorem'  body='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, fuga. dolor,  adipisicing elit. Laudantium, et?' />
+    </div>
+    
+  </>
+  )
+}
+
+const BoxWork = (props) =>{
+    return(
+    <div className='py-[25px] px-4 bg-[#1d140e] rounded-xl sm:w-[378px]  w-auto h-auto m-2'>
+        <div className='my-[10px] text-center  w-1/2 '>
+            <h1 className='text-white bg-indigo-950 rounded-md py-[2px] px-[4px]'>{props.title}</h1>
         </div>
-        <div className='w-40'>
-            testing
-        </div>
-        <div className='w-40'>
-            testing
-        </div>
-        <div className='w-40'>
-            testing
-        </div>
-        <div className='w-40'>
-            testing
-        </div>
-        <div className='w-40'>
-            testing
-        </div>
-        <div className='w-40'>
-            testing
+        <div className='text-white p-2'>
+            <h1 className='py-2 text-xl'>{props.header}</h1>
+            <h1 className='font-extralight py-3'>{props.body}</h1>
         </div>
     </div>
-  )
+    )
 }
 
 export default WorkCard
