af625d4305bee5a8cbf4cb2bf0fe2a4330d76a8a
Farizirfani <farizirfani29@gmail.com>
2023-05-25 02:32:24 +0700

    fixing

--- src/pages/error/notfound.jsx
+++ src/pages/error/notfound.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='h-screen bg-[#110111]'>
+        <div className='text-center neonText font-semibold'>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1 className='nt'>404 Not Found</h1>
+        </div>
+    </div>
   )
 }
 
