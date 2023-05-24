b07d0867702426c6720241efd15c33ebc0e12ab3
Farizirfani <farizirfani29@gmail.com>
2023-05-24 23:42:43 +0700

    add contact in navbar (neon)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -39,7 +39,7 @@ return (
     <>
         <div className="mobile  top-0 sticky z-50">
             <div className={changeColor ? `bg-active ` : 'bg-nonactive'} >
-                <div className= {`navbar ${isTop ? '' : 'navbar-scrolled'} xl:flex xl:justify-between content-center px-8 py-6 transition-transform `}>
+                <div className= {`navbar ${isTop ? '' : 'navbar-scrolled'} xl:flex xl:justify-between content-center px-8 py-6 transition-transform items-center`}>
                     <div className="logo text-white text-2xl font-bold  flex justify-between  items-center">
                         <h1>LOGO</h1>
                         <button onClick={() => setIsOpen(!isOpen)} className='xl:hidden'>
@@ -47,7 +47,7 @@ return (
                             <i class={` ${isOpen ? 'block' : 'hidden'}  fa-solid fa-xmark`}></i>
                         </button>
                     </div>
-                    <div className={`${isOpen ? 'block' : 'hidden'}  xl:flex xl:justify-evenly gap-x-10 content-center`}>
+                    <div className={`${isOpen ? 'block' : 'hidden'}  xl:flex xl:justify-evenly gap-x-10 content-center items-center`}>
                         <div >
                             <NavLink onClick={scrollToTop} className='flex justify-evenly text-white hover:text-purple-600' to='/'>
                                 Home
@@ -63,10 +63,15 @@ return (
                                 Portofolio
                             </NavLink>
                         </div>
-                        <div >
+                        {/* <div >
                             <NavLink onClick={scrollToTop} className='flex justify-evenly text-white hover:text-purple-600' to='/news'>
                                 News
                             </NavLink>
+                        </div> */}
+                        <div className=''>
+                            <div className='text-white bg-[#7303C0] px-[19px] py-2 rounded-md flex justify-evenly neon'>
+                                <a href="#a">Contact</a>
+                            </div>
                         </div>
                     </div>
                 </div>
